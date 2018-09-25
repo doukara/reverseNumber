d139044774c2c30e30d25d537c78b069d38a0d1c
man55 <doukara001@gmail.com>
2018-09-25 09:25:50 +0100

    i add all files on project

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    
+    public static void main(String [] args){
+        
+        System.out.println("Enter number : ");
+        Scanner addNumber = new Scanner(System.in);
+        int number = addNumber.nextInt();
+        
+        
+            
+        System.out.print("the reverse number is : ");
+        for (int i=number;number<i;i--)
+            
+        
+        
+    
+    
+}
