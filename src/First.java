33dc241b20b26fd10550bd3cedc6e995d9c471ad
man55 <doukara001@gmail.com>
2018-09-25 09:41:58 +0100

    add revNumber function

--- src/First.java
+++ src/First.java
@@ -1,5 +1,23 @@
 
+import java.util.Scanner;
+
+
 public class First {
     
+    static int  number;
+    public static void fun(){
+    
+       System.out.println("Enter number : ");
+       Scanner addNumber = new Scanner(System.in);
+       number = addNumber.nextInt();
+        
+        
+         
+       
+       for (int i=number;number<i;i--){
+           System.out.print("the reverse number is : " + number);
+       }
+       
+    }    
     
 }
--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,10 @@ public class Main {
     
     public static void main(String [] args){
         
-        System.out.println("Enter number : ");
-        Scanner addNumber = new Scanner(System.in);
-        int number = addNumber.nextInt();
-        
-        
-            
-        System.out.print("the reverse number is : ");
-        for (int i=number;number<i;i--)
+        First.fun();
             
         
         
-    
+            }
     
 }
